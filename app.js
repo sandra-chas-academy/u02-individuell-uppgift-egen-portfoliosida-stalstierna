c63ac482529da71c02996ede7a955c35b66ce477
stalstierna <andrea.stalstierna@chasacademy.se>
2024-11-05 21:20:53 +0100

    fixat naven

--- app.js
+++ app.js
@@ -9,32 +9,21 @@ const homeText = document.querySelector(".home_text");
 window.addEventListener("scroll", function () {
   if (document.documentElement.clientWidth >= 800) {
     if (window.scrollY > 50) {
-      navBar.classList = "navbarScroll";
+      navBar.classList.add("navbarScroll");
       console.log("hej");
     }
-
-    // if (window.scrollY < 300) {
-    //     navBar.classList = ("navbarScroll");
-    //     console.log("andrea")
-    // }
+    if (window.scrollY < 50) {
+        navBar.classList.remove("navbarScroll");
+        console.log("hej");
+      }
   }
-
-  // if (window.scrollY > 300) {
-  //     if (document.documentElement.clientWidth > 800 ) {
-  //     navBar.classList = ("navbarScroll");
-  //     console.log("hej");
-  //     }
-  // }
-
-  // if (document.documentElement.clientWidth > 800 ) {
-  //     navBar.classList.add = ("navbarScroll")
-  // }
 });
 
 darkmodeButton.addEventListener("click", function () {
   console.log("hej");
   darkmodeButton.style.justifyContent = "flex-end";
 });
+
 
 selfie.addEventListener("mouseover", function () {
   if (document.documentElement.clientWidth >= 800) {
