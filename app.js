8f404f686bca13ccbbd21cc4127faaf98bbd4828
stalstierna <andrea.stalstierna@chasacademy.se>
2024-11-05 19:59:27 +0100

    animation home

--- app.js
+++ app.js
@@ -3,36 +3,43 @@ const aboutMeImg = document.getElementById("about_me_img");
 const darkmodeButton = document.getElementById("darkmode");
 const darkmodeButtonButton = document.querySelector(".darkmode_button");
 
+const selfie = document.querySelector(".home_img");
+const homeText = document.querySelector(".home_text");
 
-window.addEventListener("scroll", function (){
-    if (document.documentElement.clientWidth > 800 ) {
-
-        if (window.scrollY > 50) {
-        navBar.classList = ("navbarScroll");
-        console.log("hej");
-        }
-
-        // if (window.scrollY < 300) {
-        //     navBar.classList = ("navbarScroll");
-        //     console.log("andrea")
-        // }
-
+window.addEventListener("scroll", function () {
+  if (document.documentElement.clientWidth >= 800) {
+    if (window.scrollY > 50) {
+      navBar.classList = "navbarScroll";
+      console.log("hej");
     }
 
-   
-    // if (window.scrollY > 300) {
-    //     if (document.documentElement.clientWidth > 800 ) {
+    // if (window.scrollY < 300) {
     //     navBar.classList = ("navbarScroll");
-    //     console.log("hej");
-    //     }
+    //     console.log("andrea")
     // }
+  }
 
-    // if (document.documentElement.clientWidth > 800 ) {
-    //     navBar.classList.add = ("navbarScroll")
-    // }
-})
+  // if (window.scrollY > 300) {
+  //     if (document.documentElement.clientWidth > 800 ) {
+  //     navBar.classList = ("navbarScroll");
+  //     console.log("hej");
+  //     }
+  // }
+
+  // if (document.documentElement.clientWidth > 800 ) {
+  //     navBar.classList.add = ("navbarScroll")
+  // }
+});
 
 darkmodeButton.addEventListener("click", function () {
-console.log("hej");
-darkmodeButton.style.justifyContent = "flex-end";
+  console.log("hej");
+  darkmodeButton.style.justifyContent = "flex-end";
+});
+
+selfie.addEventListener("mouseover", function () {
+  if (document.documentElement.clientWidth >= 800) {
+    homeText.classList.add("home_animation");
+    console.log("aaaa");
+  }
+  
 });
